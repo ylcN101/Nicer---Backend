bf40e5c90557ae75a30abe067140857230c977e6
Lidor Tal <lidortald@gmail.com>
2023-03-31 18:31:36 +0300

    .

--- server.js
+++ server.js
@@ -1,57 +1,59 @@
-const express = require('express')
-const cors = require('cors')
-const path = require('path')
-const cookieParser = require('cookie-parser')
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const cookieParser = require("cookie-parser");
 
-const app = express()
-const http = require('http').createServer(app)
+const app = express();
+const http = require("http").createServer(app);
 
 // Express App Config
-app.use(cookieParser())
-app.use(express.static('public'))
-app.use(express.json())
+app.use(cookieParser());
+app.use(express.static("public"));
+app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname, 'public')))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "public")));
 } else {
   const corsOptions = {
     // Make sure origin contains the url your frontend is running on
     origin: [
-      'http://127.0.0.1:5173',
-      'http://localhost:5173',
-      'http://127.0.0.1:3000',
-      'http://localhost:3000',
+      "http://127.0.0.1:5173",
+      "http://127.0.0.1:5174",
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://127.0.0.1:3000",
+      "http://localhost:3000",
     ],
     credentials: true,
-  }
-  app.use(cors(corsOptions))
+  };
+  app.use(cors(corsOptions));
 }
 
-const authRoutes = require('./api/auth/auth.routes')
-const userRoutes = require('./api/user/user.routes')
-const reviewRoutes = require('./api/review/review.routes')
-const gigRoutes = require('./api/gig/gig.routes')
-const { setupSocketAPI } = require('./services/socket.service')
+const authRoutes = require("./api/auth/auth.routes");
+const userRoutes = require("./api/user/user.routes");
+const reviewRoutes = require("./api/review/review.routes");
+const gigRoutes = require("./api/gig/gig.routes");
+const { setupSocketAPI } = require("./services/socket.service");
 
 // routes
-const setupAsyncLocalStorage = require('./middlewares/setupAls.middleware')
-app.all('*', setupAsyncLocalStorage)
+const setupAsyncLocalStorage = require("./middlewares/setupAls.middleware");
+app.all("*", setupAsyncLocalStorage);
 
-app.use('/api/auth', authRoutes)
-app.use('/api/user', userRoutes)
-app.use('/api/review', reviewRoutes)
-app.use('/api/gig', gigRoutes)
-setupSocketAPI(http)
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/review", reviewRoutes);
+app.use("/api/gig", gigRoutes);
+setupSocketAPI(http);
 
 // Make every server-side-route to match the index.html
 // so when requesting http://localhost:3030/index.html/gig/123 it will still respond with
 // our SPA (single page app) (the index.html file) and allow vue/react-router to take it from there
-app.get('/**', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
+app.get("/**", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
-const logger = require('./services/logger.service')
-const port = process.env.PORT || 3031
+const logger = require("./services/logger.service");
+const port = process.env.PORT || 3031;
 http.listen(port, () => {
-  logger.info('Server is running on port: ' + port)
-})
+  logger.info("Server is running on port: " + port);
+});
