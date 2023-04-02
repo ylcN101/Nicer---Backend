c4d10ca36827d9131eeee8c52fdd9efd643abdb1
Lidor Tal <lidortald@gmail.com>
2023-04-02 12:01:18 +0300

    gigs !!

--- api/gig/gig.service.js
+++ api/gig/gig.service.js
@@ -1,52 +1,52 @@
-const dbService = require('../../services/db.service')
-const logger = require('../../services/logger.service')
-const utilService = require('../../services/util.service')
-const ObjectId = require('mongodb').ObjectId
+const dbService = require("../../services/db.service");
+const logger = require("../../services/logger.service");
+const utilService = require("../../services/util.service");
+const ObjectId = require("mongodb").ObjectId;
 
 async function query(filterBy) {
   try {
-    const criteria = _buildCriteria(filterBy)
-    console.log(criteria)
-    const collection = await dbService.getCollection('gig')
-    var gigs = await collection.find(criteria).toArray()
-    console.log(gigs)
-    return gigs
+    const criteria = _buildCriteria(filterBy);
+    console.log(criteria);
+    const collection = await dbService.getCollection("gig");
+    var gigs = await collection.find(criteria).toArray();
+    console.log(gigs);
+    return gigs;
   } catch (err) {
-    logger.error('cannot find gigs', err)
-    throw err
+    logger.error("cannot find gigs", err);
+    throw err;
   }
 }
 
 async function getById(gigId) {
   try {
-    const collection = await dbService.getCollection('gig')
-    const gig = collection.findOne({ _id: ObjectId(gigId) })
-    return gig
+    const collection = await dbService.getCollection("gig");
+    const gig = collection.findOne({ _id: ObjectId(gigId) });
+    return gig;
   } catch (err) {
-    logger.error(`while finding gig ${gigId}`, err)
-    throw err
+    logger.error(`while finding gig ${gigId}`, err);
+    throw err;
   }
 }
 
 async function remove(gigId) {
   try {
-    const collection = await dbService.getCollection('gig')
-    await collection.deleteOne({ _id: ObjectId(gigId) })
-    return gigId
+    const collection = await dbService.getCollection("gig");
+    await collection.deleteOne({ _id: ObjectId(gigId) });
+    return gigId;
   } catch (err) {
-    logger.error(`cannot remove gig ${gigId}`, err)
-    throw err
+    logger.error(`cannot remove gig ${gigId}`, err);
+    throw err;
   }
 }
 
 async function add(gig) {
   try {
-    const collection = await dbService.getCollection('gig')
-    await collection.insertOne(gig)
-    return gig
+    const collection = await dbService.getCollection("gig");
+    await collection.insertOne(gig);
+    return gig;
   } catch (err) {
-    logger.error('cannot insert gig', err)
-    throw err
+    logger.error("cannot insert gig", err);
+    throw err;
   }
 }
 
@@ -55,82 +55,82 @@ async function update(gig) {
     const gigToSave = {
       title: gig.title,
       price: gig.price,
-    }
-    const collection = await dbService.getCollection('gig')
-    await collection.updateOne({ _id: ObjectId(gig._id) }, { $set: gigToSave })
-    return gig
+    };
+    const collection = await dbService.getCollection("gig");
+    await collection.updateOne({ _id: ObjectId(gig._id) }, { $set: gigToSave });
+    return gig;
   } catch (err) {
-    logger.error(`cannot update gig ${gigId}`, err)
-    throw err
+    logger.error(`cannot update gig ${gigId}`, err);
+    throw err;
   }
 }
 
 async function addGigMsg(gigId, msg) {
   try {
-    msg.id = utilService.makeId()
-    const collection = await dbService.getCollection('gig')
+    msg.id = utilService.makeId();
+    const collection = await dbService.getCollection("gig");
     await collection.updateOne(
       { _id: ObjectId(gigId) },
       { $push: { msgs: msg } }
-    )
-    return msg
+    );
+    return msg;
   } catch (err) {
-    logger.error(`cannot add gig msg ${gigId}`, err)
-    throw err
+    logger.error(`cannot add gig msg ${gigId}`, err);
+    throw err;
   }
 }
 
 async function removeGigMsg(gigId, msgId) {
   try {
-    const collection = await dbService.getCollection('gig')
+    const collection = await dbService.getCollection("gig");
     await collection.updateOne(
       { _id: ObjectId(gigId) },
       { $pull: { msgs: { id: msgId } } }
-    )
-    return msgId
+    );
+    return msgId;
   } catch (err) {
-    logger.error(`cannot add gig msg ${gigId}`, err)
-    throw err
+    logger.error(`cannot add gig msg ${gigId}`, err);
+    throw err;
   }
 }
 
 function _buildCriteria(
   filterBy = {
-    title: '',
+    title: "",
     minPrice: 0,
     maxPrice: 100000,
     daysToDeliver: 0,
-    categoryId: '',
+    categoryId: "",
     owner: null,
   }
 ) {
   const { title, minPrice, maxPrice, daysToDeliver, categoryId, owner } =
-    filterBy
-  const criteria = {}
+    filterBy;
+  const criteria = {};
   if (title) {
-    criteria.title = { $regex: new RegExp(title, 'i') }
+    criteria.title = { $regex: new RegExp(title, "i") };
   }
   if (minPrice) {
-    criteria.price = { $gte: +minPrice }
+    criteria.price = { $gte: +minPrice };
   }
   if (maxPrice) {
-    criteria.price = { $lte: +maxPrice }
+    criteria.price = { $lte: +maxPrice };
   }
   if (daysToDeliver) {
-    criteria.daysToDeliver = { $lte: +daysToDeliver }
-    if (daysToDeliver === 'any') {
-      delete criteria.daysToDeliver
+    criteria.daysToDeliver = { $lte: +daysToDeliver };
+    if (daysToDeliver === "any") {
+      delete criteria.daysToDeliver;
     }
   }
   if (categoryId) {
-    criteria.categories = { $elemMatch: { $eq: categoryId } }
+    criteria.categories = { $elemMatch: { $eq: categoryId } };
   }
 
   if (minPrice && maxPrice) {
-    criteria.price = { $gte: +minPrice, $lte: +maxPrice }
+    criteria.price = { $gte: +minPrice, $lte: +maxPrice };
   }
 
-  return criteria
+  return criteria;
 }
 
 module.exports = {
@@ -141,4 +141,4 @@ module.exports = {
   update,
   addGigMsg,
   removeGigMsg,
-}
+};
